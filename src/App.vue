5ee4db127e0fd4063efcbedf97c30c99c5bf46c6
Lucas Santos <lucsantosdev@gmail.com>
2026-05-06 15:34:14 -0300

    feat: add transactionList display in global state as props

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,20 @@
-<script>
-  import Header from './components/Header.vue';
-  import Balance from './components/Balance.vue';
-  import IncomeExpenses from './components/IncomeExpenses.vue';
-  import TransactionList from './components/TransactionList.vue';
-  import AddTransaction from './components/AddTransaction.vue';
+<script setup>
 
-  export default {
-    name: 'App',
-    components: {
-      Header,
-      Balance,
-      IncomeExpenses,
-      TransactionList,
-      AddTransaction
-    }
-  }
+import Header from './components/Header.vue';
+import Balance from './components/Balance.vue';
+import IncomeExpenses from './components/IncomeExpenses.vue';
+import TransactionList from './components/TransactionList.vue';
+import AddTransaction from './components/AddTransaction.vue';
+
+import { ref } from 'vue';
+
+const transactions = ref([
+    { id: 1, text: 'Salary', amount: 5400 },
+    { id: 2, text: 'Rent', amount: -400 },
+    { id: 3, text: 'Books', amount: -40 },
+    { id: 4, text: 'Commission', amount: 150 }
+]);
+
 </script>
 
 <template>
@@ -22,7 +22,7 @@
   <div class="container">
     <Balance />
     <IncomeExpenses />
-    <TransactionList />
+    <TransactionList :transactions="transactions" />
     <AddTransaction />
   </div>
 </template>
--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -1,11 +1,27 @@
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+    transactions: {
+        type: Array,
+        required: true
+    }
+});
+
+</script>
+
 <template>
     <h3>History</h3>
     <ul id="list" class="list">
+        <!-- For testing purposes, you can add/remove transactions here 
         <li class="minus">
             Cash <span>- $400</span><button class="delete-btn">x</button>
         </li>
         <li class="plus">
             Cash <span>+ $400</span><button class="delete-btn">x</button>
+        </li> -->
+        <li v-for="transaction in transactions" v-bind:key="transaction.id" :class="transaction.amount < 0 ? 'minus' : 'plus'">
+            {{ transaction.text }} <span>$ {{ transaction.amount }}</span><button class="delete-btn">x</button>
         </li>
     </ul>
 </template>
