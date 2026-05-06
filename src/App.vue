aeebbe516f20c5f63e5e6df04c6f96900467eb60
Lucas Santos <lucsantosdev@gmail.com>
2026-05-06 15:00:17 -0300

    feat: create and import components

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,30 @@
+<script>
+  import Header from './components/Header.vue';
+  import Balance from './components/Balance.vue';
+  import IncomeExpenses from './components/IncomeExpenses.vue';
+  import TransactionList from './components/TransactionList.vue';
+  import AddTransaction from './components/AddTransaction.vue';
+
+  export default {
+    name: 'App',
+    components: {
+      Header,
+      Balance,
+      IncomeExpenses,
+      TransactionList,
+      AddTransaction
+    }
+  }
+</script>
+
 <template>
-  
+  <Header />
+  <div class="container">
+    <Balance />
+    <IncomeExpenses />
+    <TransactionList />
+    <AddTransaction />
+  </div>
 </template>
+
 
---
+++ src/components/AddTransaction.vue
@@ -0,0 +1,14 @@
+<template>
+    <h3>Add New Transaction</h3>
+    <form id="form">
+        <div class="form-control">
+            <label for="text">Text</label>
+            <input type="text" id="text" placeholder="Enter text..." />
+        </div>
+        <div class="form-control">
+            <label for="amount">Amount <br /> (negative as expense, positive as income)</label>
+            <input type="number" id="amount" placeholder="Enter amount..." />
+        </div>
+        <button class="btn">Add transaction</button>
+    </form>
+</template>
